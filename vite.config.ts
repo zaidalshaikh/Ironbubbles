3943e37b9ff8bbe3df6bc230d04551967930d78c
zaidalshaikh <zaidalshaiskh@gmail.com>
2026-03-28 02:12:55 +0300

    Update base property in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
+base: "Ironbubbles",
 
 export default defineConfig({
   plugins: [
