eace34626699a615aebbfe27928732cb8b6178f6
zaidalshaikh <zaidalshaiskh@gmail.com>
2026-03-28 12:11:21 +0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
+
 
 
 export default defineConfig({
-  base: "Ironbubbles",
+  base: "/Ironbubbles/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
     tailwindcss(),
-    metaImagesPlugin(),
+ 
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
